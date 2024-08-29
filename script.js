3d40246ed7eaf336e4fe13e6932472cb083ed966
aim-salam <aimsalam.com@gmail.com>
2024-08-29 11:13:17 +0800

    Created an async functions: getListTodo

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
+let todoList = []
+
+getListTodo().then((result)=>{
+  todoList = [...result]
+  filterListTodoById(3).then((result)=>{
+    console.log(result)
+  })
+
+}).catch((err)=>console.log(`Error on script.js: ${result}`));
+
+
+
 async function getListTodo() {
-  const response = await fetch('https://jsonplaceholder.typicode.com/todos');
-  const json = await response.json();
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos');
+    const json = await response.json();
+    return json
+  } catch (error) {
+    console.log(`Error on getListTodo function: ${error}`)
+  }
+  }
+  
+  
+  async function filterListTodoById(id){
+    const filteredTodoList = []
 
-  console.log(json);
-}
+    for (const element of todoList) {
+      element.userId ===id ?filteredTodoList.push(element):null
+      
+    }
+   return filteredTodoList
+  }
 
-getListTodo();
+
+// const object = {completed: false,id:2,title: "quis ut nam facilis et officia qui",userId: 1}
